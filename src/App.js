6c486c6dc27127dce274cda3affa7bfc572950cf
Jenni Mämmi <30490109+JenniMammi@users.noreply.github.com>
2021-10-31 12:13:05 +0200

    Initial commit, added minkkiface

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import minkkiface from "./minkkiface.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={minkkiface} className="minkkiface" alt="minkki img-fluid" />
+      </header>
+    </div>
+  );
+}
