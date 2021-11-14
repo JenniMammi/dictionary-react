39f570a4f25044c2bd53e25ece5bbed41121e84c
Jenni Mämmi <30490109+JenniMammi@users.noreply.github.com>
2021-11-14 11:27:55 +0200

    Fixed another missing alt

--- src/Photos.js
+++ src/Photos.js
@@ -16,7 +16,11 @@ export default function Photos(props) {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <img src={photo.src.landscape} className="img-fluid" />
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt="image"
+                  />
                 </a>
               </div>
             );
