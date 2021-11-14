faf59dbf7e4a9c71dd0171dd6bcb6164616aad5a
Jenni Mämmi <30490109+JenniMammi@users.noreply.github.com>
2021-11-14 11:34:36 +0200

    Final fixes, then ready

--- src/Photos.js
+++ src/Photos.js
@@ -14,7 +14,7 @@ export default function Photos(props) {
                   <img
                     src={photo.src.landscape}
                     className="img-fluid"
-                    alt="landscape"
+                    alt={photo.src.photographer}
                   />
                 </a>
               </div>
